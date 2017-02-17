21c1b748491b581fc9efa67b99f67606a3086bdc
Yulian <darkreptilez@gmail.com>
2017-02-17 16:05:50 +0200

    workshop.lua

---
+++ lua/autorun/server/workshop.lua
@@ -0,0 +1,18 @@
+resource.AddWorkshop("109643223") -- 3D2D Textscreens
+resource.AddWorkshop("104482086") -- [Official] Precision Tool
+resource.AddWorkshop("121961644") -- NYPD 1940's Playermodels
+resource.AddWorkshop("107982746") -- rp_downtown_v2
+resource.AddWorkshop("104815552") -- SmartSnap
+resource.AddWorkshop("106944414") -- RT Camera
+resource.AddWorkshop("310289701") -- rp_1944RPG_Town_v2a
+resource.AddWorkshop("149491742") -- Mafia Playermodels
+resource.AddWorkshop("283211438") -- Don Corleone Playermodel
+resource.AddWorkshop("104479831") -- Stacker STool
+resource.AddWorkshop("104634865") -- No Collide World
+resource.AddWorkshop("220336312") -- PermaProps
+resource.AddWorkshop("450599519") -- Selfie Cam
+resource.AddWorkshop("327281224") -- Roleplay Props
+resource.AddWorkshop("474029314") -- Magnusson Detective
+resource.AddWorkshop("128093075") -- M9K Small Arms
+resource.AddWorkshop("108176967") -- Sit Anywhere
+resource.AddWorkshop("182758912") -- Thief Playermodel
