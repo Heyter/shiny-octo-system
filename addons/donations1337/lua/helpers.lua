a1e722dcc4251f3132af837896033afdd2f72d3b
Ddone <mynameisthenekit@gmail.com>
2017-03-02 08:35:28 +0200

    Апдейт донатосистемы

--- addons/donations1337/lua/helpers.lua
+++ addons/donations1337/lua/helpers.lua
@@ -1,31 +1,25 @@
 meta = FindMetaTable("Player")
-local sendPlyQuery = db:prepare("INSERT INTO players (`steamid`, `steamid64`, `money`, `usergroup`) VALUES(?, ?, ?, ?)")
-
-function sendPlyQuery:onSuccess(data)
-	print("Rows inserted successfully!")
-end
-
-function sendPlyQuery:onError(err,sql)
-	print("An error occured while executing the query: " .. err)
-	print(sql)
-end
-
-local selectPlayer = db:prepare("SELECT * FROM players WHERE `steamid` = ?")
-
-function selectPlayer:onSuccess(data)
+qrs = {}
+qrs.sendPlyQueryPattern 	= "INSERT INTO players (`steamid`, `steamid64`, `money`, `usergroup`, `firstjoined`) VALUES(?, ?, ?, ?, ?)"
+qrs.selPlyQueryPattern 		= "SELECT * FROM players WHERE `steamid` = ?"
+qrs.sendLogQueryPattern		= "INSERT INTO log (`time`,`entry`) VALUES (?, ?)"
+function genericOnSuccess(q,data)
 	print("DB query returned")
 
 end
 
-function selectPlayer:onError(err, sql)
+function genericOnError(q, err, sql)
 	print("An error occured while executing the query: " .. err)
 	print(sql)
 end
 
 function meta:Process()
 	if not DBCONNECTED then return end
-	selectPlayer:setString(1,self:SteamID())
-	selectPlayer:start()
+	local 	selectPlayer = db:prepare(qrs.selPlyQueryPattern)
+			selectPlayer.onSuccess = genericOnSuccess
+			selectPlayer.onError = genericOnError
+			selectPlayer:setString(1,self:SteamID())
+			selectPlayer:start()
 	timer.Simple(0, function() 
 		if selectPlayer:isRunning() then
 		selectPlayer:wait() end
@@ -33,11 +27,25 @@ function meta:Process()
 		--print(#data)
 		--PrintTable(data)
 		if not data or #data == 0 then
-			sendPlyQuery:setString(1,self:SteamID())
-			sendPlyQuery:setNumber(2,tonumber(self:SteamID64()))
-			sendPlyQuery:setNumber(3,self:getDarkRPVar("money") or 0)
-			sendPlyQuery:setString(4,self:GetUserGroup())
-			sendPlyQuery:start()
+			writeDBLog("New player "..self:SteamID().." joined, adding to playerbase")
+			local 	sendPlyQuery = db:prepare(qrs.sendPlyQueryPattern)
+					sendPlyQuery.onSuccess = genericOnSuccess
+					sendPlyQuery.onError = genericOnError
+					sendPlyQuery:setString(1,self:SteamID())
+					sendPlyQuery:setNumber(2,tonumber(self:SteamID64()))
+					sendPlyQuery:setNumber(3,self:getDarkRPVar("money") or 0)
+					sendPlyQuery:setString(4,self:GetUserGroup())
+					sendPlyQuery:setNumber(5,os.time())
+					sendPlyQuery:start()
 		end
 	end)
 end
+
+function writeDBLog(str)
+	local 	sendlog = db:prepare(qrs.sendLogQueryPattern)
+			sendlog.onSuccess = genericOnSuccess
+			sendlog.onError = genericOnError
+			sendlog:setNumber(1,os.time())
+			sendlog:setString(2,str)
+			sendlog:start()
+	end
