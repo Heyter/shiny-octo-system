6091e72345eb672f0d93733a1c80500ebec9a876
EnigmaticOctopus <mynameisthenekit@gmail.com>
2017-03-02 19:28:07 +0200

    Система смены донатерских статусов (#35)

--- addons/donations1337/lua/helpers.lua
+++ addons/donations1337/lua/helpers.lua
@@ -1,10 +1,17 @@
 UNIX_1_DAY =  86400
 function genericOnSuccess(q,data)
-	print("DB query returned again")
+	--print("DB query returned again")
 
 end
 
 function genericOnError(q, err, sql)
+	ply = {}
+	for k,v in player.GetAll() do
+		if isAdmin(v) then
+			table.insert(ply,v)
+		end
+	end
+	DarkRP.notify(ply,1,10,"Ошибки в SQL! Передай это ближайшему доступному кодеру.")
 	print("An error occured while executing the query: " .. err)
 	print(sql)
 end
@@ -28,6 +35,35 @@ function prepareAndRun(query, ...)
 	return toRun
 end
 
+function prepareNotRun(query, ...)
+	if not DBCONNECTED then return false end
+	local args = {...}
+	local toRun = db:prepare(query)
+	toRun.onSuccess = genericOnSuccess
+	toRun.onError = genericOnError
+	--PrintTable(args)
+	for k,v in ipairs(args) do
+		if type(v) == "string" then
+			toRun:setString(k,v)
+		end
+		if type(v) == "number" then
+			toRun:setNumber(k,v)
+		end
+	end
+	return toRun
+end
+
+function addOrUpdateRemove(ply64,time,type)
+	local sel = prepareAndRun(SQLPatterns.checkPending,ply64,os.time(),type)
+	if sel:isRunning() then sel:wait() end
+	data = sel:getData()
+	if #data == 0 then
+		return prepareNotRun(SQLPatterns.addPending,ply64,time,type)
+	else
+		return prepareNotRun(SQLPatterns.updatePending,time,data[1]['id'])
+	end
+
+end
 function writeDBLog(str)
 	prepareAndRun(SQLPatterns.sendLog, os.time(), str)
 	end
--- addons/donations1337/lua/processor.lua
+++ addons/donations1337/lua/processor.lua
@@ -1,5 +1,6 @@
 function processPendings()
-	local pendings = prepareAndRun(SQLPatterns.getpending)
+	if not DBCONNECTED then return end
+	local pendings = prepareAndRun(SQLPatterns.getpending,os.time())
 	if pendings:isRunning() then pendings:wait() end
 	local data = pendings:getData()
 	print("Processing ".. #data.." pendings now")
@@ -16,11 +17,41 @@ function processPendings()
 					query.onSuccess = function(q) q.affectedPlayer:addMoney(q.moneyValue) end
 					query.onError = gnericOnError
 					query:start()
-			writeDBLog("Added ".. money.." dolalrs to"..v.steamid64)
+			writeDBLog("Added ".. money.." dolalrs to "..v.steamid64)
+		end
+		if v.type == "addgroup" then
+			local ply64 = v.steamid64
+			local ply = util.SteamIDFrom64(v.steamid64)
+			local time = os.time() + UNIX_1_DAY*v.amount
+			local group = v.addargs
+			local id = v.id
+			local trans = db:createTransaction()
+				trans:addQuery(prepareNotRun(SQLPatterns.updatePStatus,id))
+				trans:addQuery(addOrUpdateRemove(ply64,time,"removegroup"))
+				trans:addQuery(prepareNotRun(SQLPatterns.updateUGroup,group,ply64))
+				trans.ply = ply
+				trans.group = group
+				trans.onSuccess = function(tr) ULib.ucl.addUser(tr.ply,{},{},tr.group) end
+				trans.onError = genericOnError
+				trans:start()
+			writeDBLog("Modified ".. ply .." group to ".. group)
+		end
+		if v.type == "removegroup" then
+			local ply64 = v.steamid64
+			local ply = util.SteamIDFrom64(v.steamid64)
+			local id = v.id
+			local trans = db:createTransaction()
+				trans:addQuery(prepareNotRun(SQLPatterns.updatePStatus,id))
+				trans:addQuery(prepareNotRun(SQLPatterns.updateUGroup,"user",ply64))
+				trans.ply = ply
+				trans.onSuccess = function(q) ULib.ucl.removeUser(q.ply) end
+				trans.onError = genericOnError
+				trans:start()
+				writeDBLog("Removed access from"..ply)
 		end
 
 	end
 
 end
 
-timer.Create("SQLprocessor",60*5,0,processPendings)
+timer.Create("SQLprocessor",30,0,processPendings)
