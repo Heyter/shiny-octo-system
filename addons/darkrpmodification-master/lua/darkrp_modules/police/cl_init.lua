c5c992eb17146ae770365a171593272ffaa5eadd
Yulian <darkreptilez@gmail.com>
2017-03-08 12:03:26 +0200

    police waypoint fixed

--- addons/darkrpmodification-master/lua/darkrp_modules/police/cl_init.lua
+++ addons/darkrpmodification-master/lua/darkrp_modules/police/cl_init.lua
@@ -8,9 +8,10 @@ net.Receive( "CombineRequest",
 	 --print( "I got it!2" )
 	 local vec = net.ReadVector()
 	 local i = #ply.markers+1
+	 vec.z = vec.z + 40 
 	 ply.markers[i] = vec
 
-	 timer.Simple( 30, function() 
+	 timer.Simple( 60, function() 
 	 	for i,k in pairs(ply.markers) do
 	 		if k==vec then
 	 			table.remove(ply.markers,i)
@@ -31,12 +32,11 @@ function drawmarkers()
 	
 	for i,pos in ipairs(ply.markers) do
 		dist = pos:Distance(EyePos())
-		pos.z = pos.z + 40
 		pos = pos:ToScreen()
 		surface.SetDrawColor(255,255,255)
 		surface.SetMaterial( Material( "materials/Waypoint.png" ) )
-		surface.DrawTexturedRect(pos.x, pos.y, 50, 77)
-		draw.SimpleText( math.ceil(dist),"DermaDefault", pos.x+25, pos.y+80, Color( 255, 255, 255, 255 ),TEXT_ALIGN_CENTER,TEXT_ALIGN_CENTER)
+		surface.DrawTexturedRect(pos.x, pos.y, 25, 38.5)
+		draw.SimpleText( math.ceil(dist),"DermaDefault", pos.x+13, pos.y+42, Color( 255, 255, 255, 255 ),TEXT_ALIGN_CENTER,TEXT_ALIGN_CENTER)
 	end
 end
 hook.Add("HUDPaint", "DrawMarkers", drawmarkers)
